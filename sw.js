c9af09234599c99176010684e6da9583011a0b23
servicefireworkx <fireworkx.service@gmail.com>
2026-07-15 05:56:19 +0000

    addition on 2024-06-19 15:30:00 +0000

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'servicing-app-v3';
+const CACHE_NAME = 'servicing-app-v4';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
